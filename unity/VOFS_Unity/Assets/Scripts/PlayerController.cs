a21f7489b2d7d2f9e36b6ed28c2b9977b24e854f
lichingchester <liching.chester@gmail.com>
2016-12-23 16:28:22 +0800

    unity project setup and testing

---
+++ unity/VOFS_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetKeyUp(KeyCode.W))
+        //{
+        //    Debug.Log("Input W");
+        //}
+    }
+
+    // use for before any physics calculation 
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * 10.0f);
+    }
+}
