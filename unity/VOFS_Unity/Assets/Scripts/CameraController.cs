cbb12d1d25a161ce01000e2c8f8d4a0ebe67e696
lichingchester <liching.chester@gmail.com>
2017-01-17 17:06:59 +0800

    rewrite the camera setting
    setting the player movement follow the camera movement

--- unity/VOFS_Unity/Assets/Scripts/CameraController.cs
+++ unity/VOFS_Unity/Assets/Scripts/CameraController.cs
@@ -8,17 +8,21 @@ public class CameraController : MonoBehaviour {
     Vector2 smoothV;
     public float sensitivity = 5.0f;
     public float smoothing = 2.0f;
-    GameObject player;    
+    GameObject player;
+    //GameObject player_parent;    
 
 	// Use this for initialization
 	void Start () {
         // mouse look
-        player = this.transform.parent.gameObject;
+        player = this.transform.parent.parent.gameObject;
+        // capsule player
+        //player_parent = this.transform.parent.parent.gameObject;
     }
 	
 	// Update is called once per frame
 	void Update () {
         // mouse look
+        //var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
         md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
@@ -28,6 +32,18 @@ public class CameraController : MonoBehaviour {
 
         transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
         player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+
+        // mouse look in the capsule
+        //var md2 = new Vector2(Input.GetAxisRaw("Mouse X"), 0);
+
+        //md2 = Vector2.Scale(md2, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        //smoothV.x = Mathf.Lerp(smoothV.x, md2.x, 1f / smoothing);
+        //smoothV.y = Mathf.Lerp(smoothV.y, md2.y, 1f / smoothing);
+        //mouseLook += smoothV;
+
+        //transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        //player_parent.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+
     }
 
     //the last frame update
